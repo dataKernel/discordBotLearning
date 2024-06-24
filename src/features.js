3f4ad0e910a9091d7106bf11ac7366763b224078
dataKernel <lancelot.saintleger@gmail.com>
2024-06-24 17:43:26 +0100

    remove interaction conditional statement in the add and sub operations

--- src/features.js
+++ src/features.js
@@ -1,22 +1,21 @@
 //-------------------------- REGISTERY SLASH COMMANDS ---------------------------------
 //function to add 2 numbers
-function    add_op(interaction, a, b)
+function    add_op(a, b)
 {
     let     res;
     let     str;
-    if(interaction.commandName == "add")
-        res = a + b;
+    
+    res = a + b;
     str = `(ADD_OP) -> Result:${res}`;
     return(str);
 }
 //function to sub 2 numbers
-function    sub_op(interaction, a, b)
+function    sub_op(a, b)
 {
     let     res;
     let     str;
 
-    if(interaction.commandName == "sub")
-        res = a - b;
+    res = a - b;
     str = `(SUB_OP) -> Result: ${res}`;
     return(str);
 }
@@ -37,23 +36,33 @@ function    read_commands(client, arrayCommands)
     });
 }
 
+//function to generate args array based on interaction object
+function    get_interaction_args(interaction)
+{
+    const   array = [];
+
+    array[0] = interaction.options.get("a").value;
+    array[1] = interaction.options.get("b").value;
+
+    return(array); 
+}
+
 //function to generate all slash commands
-//important penser a refacto la fonction avec des objets pr les slash cmd
 function    read_slash_commands(client, arraySlashCommands)
 {
     client.on('interactionCreate', (interaction) => 
     {   
+        const   interactionArgs = get_interaction_args(interaction);
         if (!interaction.isChatInputCommand())
             return;
-        
+        console.log(interactionArgs);
     });
 }
 
 //functionalities export with module
 module.exports = 
 {
-    add_op,
-    sub_op,
-    read_commands, 
-    read_slash_commands
+    add_op, sub_op,
+    get_interaction_args,
+    read_commands, read_slash_commands,
 };

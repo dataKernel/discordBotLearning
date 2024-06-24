f73890e8bf8ed97d16336415014417b915babe2a
dataKernel <lancelot.saintleger@gmail.com>
2024-06-24 14:53:13 +0100

    refactoring the calc function & updating the params management

--- src/features.js
+++ src/features.js
@@ -1,3 +1,26 @@
+//-------------------------- REGISTERY SLASH COMMANDS ---------------------------------
+//function to add 2 numbers
+function    add_op(interaction, a, b)
+{
+    let     res;
+    let     str;
+    if(interaction.commandName == "add")
+        res = a + b;
+    str = `(ADD_OP) -> Result:${res}`;
+    return(str);
+}
+//function to sub 2 numbers
+function    sub_op(interaction, a, b)
+{
+    let     res;
+    let     str;
+
+    if(interaction.commandName == "sub")
+        res = a - b;
+    str = `(SUB_OP) -> Result: ${res}`;
+    return(str);
+}
+
 //function to generate all basic commands
 function    read_commands(client, arrayCommands)
 {
@@ -6,7 +29,8 @@ function    read_commands(client, arrayCommands)
         //secure that the bot doesn't reply to himeself recursively
         if (msg.author.bot)
             return;
-        for (const key in arrayCommands) {
+        for (const key in arrayCommands) 
+        {
             if (msg.content == key)
                 msg.reply(arrayCommands[key]);
         }
@@ -18,44 +42,18 @@ function    read_commands(client, arrayCommands)
 function    read_slash_commands(client, arraySlashCommands)
 {
     client.on('interactionCreate', (interaction) => 
-    {
-        let     a, b;
+    {   
         if (!interaction.isChatInputCommand())
             return;
-        for(const val of arraySlashCommands[0])
-        {
-            if(interaction.commandName == val)
-            {
-                a = interaction.options.get("a").value;
-                b = interaction.options.get("b").value;
-            }
-        }
         
-        switch(interaction.commandName)
-        {
-            case "add":
-                interaction.reply(`(ADD_OP) -> Result: ${a + b}`);
-                break;
-            case "sub":
-                interaction.reply(`(SUB_OP) -> Result: ${a - b}`);
-                break;
-            case "mul":
-                interaction.reply(`(MUL_OP) -> Result: ${a * b}`);
-                break;
-            case "div":
-                interaction.reply(`(DIV_OP) -> Result: ${a / b}`);
-                break;
-            case "mod":
-                interaction.reply(`(MOD_OP) -> Result: ${a % b}`);
-                break;
-            case "embed":
-                interaction.reply("embed fonction détéctée.. (test OK)");
-                break;
-            default:
-                interaction.reply("error refacto switch (test KO)");
-        }
     });
 }
 
 //functionalities export with module
-module.exports = {read_commands, read_slash_commands};
+module.exports = 
+{
+    add_op,
+    sub_op,
+    read_commands, 
+    read_slash_commands
+};
